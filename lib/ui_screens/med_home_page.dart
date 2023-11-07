1e4ff08143e2fe1c75a216db1427e47317bf0cb7
gauravt <gauravt@dewsolutions.in>
2023-11-07 17:07:58 +0530

    3rd commit

--- lib/ui_screens/med_home_page.dart
+++ lib/ui_screens/med_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import '../widgets/daynamic_card.dart';
 
 
@@ -6,48 +7,49 @@ class MedHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            DynamicCard(
-              imagePath: 'assets/images/lab_test_image.png',
-              header: 'Book Lab Test',
-              description: 'Schedule a lab test appointment.',
-              onTap: () {
-                // Navigate to the lab test booking screen
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(builder: (context) => LabTestScreen()),
-                // );
-              },
+      body: Column(
+        children: [
+          // Add the first banner loaded from the internet
+          Container(
+            height: 300, // Set the desired height for the banner
+            child: Image.network(
+              'https://img.freepik.com/free-vector/hospital-healthcare-service-sale-banner_23-2150394136.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1698969600&semt=ais', // Replace with the actual URL of the banner
+              fit: BoxFit.cover,
             ),
-            DynamicCard(
-              imagePath: 'assets/images/lab_test_image.png',
-              header: 'Buy Medicines Online',
-              description: 'Order medicines online with delivery.',
-              onTap: () {
-                // Navigate to the online medicines screen
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(builder: (context) => MedicinesScreen()),
-                // );
-              },
+          ),
+
+          // Dynamic cards go here
+          Expanded(
+            child: ListView(
+              children: [
+                DynamicCard(
+                  imagePath: 'assets/images/lab_test_img.png',
+                  header: 'Book Lab Test',
+                  description: 'Schedule a lab test appointment.',
+                  onTap: () {
+                    // Navigate to the lab test booking screen
+                  },
+                ),
+                DynamicCard(
+                  imagePath: 'assets/images/med_buy.jpeg',
+                  header: 'Buy Medicines Online',
+                  description: 'Order medicines online with delivery.',
+                  onTap: () {
+                    // Navigate to the online medicines screen
+                  },
+                ),
+                DynamicCard(
+                  imagePath: 'assets/images/doctor_img.jpeg',
+                  header: "Book Doctor's Appointment",
+                  description: 'Find and schedule an appointment with a doctor.',
+                  onTap: () {
+                    // Navigate to the doctor's appointment booking screen
+                  },
+                ),
+              ],
             ),
-            DynamicCard(
-              imagePath: 'assets/images/lab_test_image.png',
-              header: "Book Doctor's Appointment",
-              description: 'Find and schedule an appointment with a doctor.',
-              onTap: () {
-                // Navigate to the doctor's appointment booking screen
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(builder: (context) => DoctorAppointmentScreen()),
-                // );
-              },
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
